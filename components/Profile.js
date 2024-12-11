920f58ed0fa576ff3bbe8ffbbc486a0c3819b532
yp9435 <147942578+yp9435@users.noreply.github.com>
2024-12-11 15:09:35 +0530

    final commit

--- components/Profile.js
+++ components/Profile.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Card from "./Card";
 
-const Profile = () => {
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <section className='w-full'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{name} Profile</span>
+      </h1>
+      <p className='desc text-left'>{desc}</p>
 
-export default Profile
+      <div className='mt-10 prompt_layout'>
+        {data.map((post) => (
+          <Card
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
