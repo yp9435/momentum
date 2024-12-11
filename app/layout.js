25b90fa4cad8f905d77c0023d0603134162ebcf9
yp9435 <147942578+yp9435@users.noreply.github.com>
2024-12-11 16:52:15 +0530

    footer

--- app/layout.js
+++ app/layout.js
@@ -5,21 +5,27 @@ import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Momentum",
-  description: "Discover & Share habit-building strategies, personal growth experiments, and success stories to inspire and support your self-improvement journey.",
+  description:
+    "Discover & Share habit-building strategies, personal growth experiments, and success stories to inspire and support your self-improvement journey.",
 };
 
 const RootLayout = ({ children }) => (
-  <html lang='en'>
+  <html lang="en">
     <body>
       <Provider>
-        <div className='main'>
-          <div className='gradient' />
+        <div className="main">
+          <div className="gradient" />
         </div>
 
-        <main className='app'>
+        <main className="app">
           <Nav />
           {children}
         </main>
+
+        {/* Footer */}
+        <footer className="footer">
+          <p>&copy; Yeshaswi Prakash 2024</p>
+        </footer>
       </Provider>
     </body>
   </html>
